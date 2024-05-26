d5782df7c9eefdad068654289f9057e58425ddb9
Nathan1188101 <njsecord@lakeheadu.ca>
2024-05-26 15:50:48 -0400

    trying to get the random question route working

--- index.js
+++ index.js
@@ -26,6 +26,26 @@ app.get('/questions/:category', async (req, res) => {
     }
 })
 
+app.get('/questions/random', async (req, res) => {
+    const { category } = req.query //get the category from the query parameters
+    console.log('This is the category: ', category) //log the category to the console 
+    try{
+
+        const count = await Question.countDocuments({ category })
+        const random = Math.floor(Math.random() * count ) //generating a random numbe between 0 and the number of questions in the category
+        const question = await Question.findOne({ category }).skip(random)//find a random question with the category that was passed in the request
+
+        if(!question){
+            return res.status(404).json({message: 'No questions found'}) //send a status code and a message if no questions are found
+        }
+        res.status(200).json(question) //send the question as a json object 
+    }
+    catch(err){
+        res.status(500).json({message: err.message}) //send a status code and a message if an error occurs
+    }
+
+})
+
 
 //routes for tshirts (this was for learning purposes)
 app.get('/tshirt', (req, res) => { //run this function when the route is requested 
